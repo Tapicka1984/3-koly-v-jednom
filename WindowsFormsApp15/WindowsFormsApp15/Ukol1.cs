12c93ead42a27336cb06efa65bffff89cc054834
the Tap <97515458+Tapicka1984@users.noreply.github.com>
2022-06-12 17:06:31 +0200

    Add files via upload

---
+++ WindowsFormsApp15/WindowsFormsApp15/Ukol1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp15
+{
+    public partial class Ukol1 : Form
+    {
+        public Ukol1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string řetezec = textBox1.Text;
+            JeAlfanumericky(řetezec);
+        }
+        public void JeAlfanumericky(string řetezec)
+        {
+            int pocetVelkych = 0;
+            int pocetMalych = 0;
+            int pocetNealfa = 0;
+            bool Alfa = false;
+
+            foreach (char x in řetezec)
+            {
+                if (((x >= 48) && (x <= 57)) || ((x >= 65) && (x <= 90)) || ((x >= 97) && (x <= 122)))
+                {
+                    if ((x >= 65) && (x <= 90))
+                    {
+                        pocetVelkych++;
+                    }
+                    if ((x >= 97) && (x <= 122))
+                    {
+                        pocetMalych++;
+                    }
+                }
+                else
+                {
+                    pocetNealfa++;
+                    Alfa = true;
+                }
+            }
+            if(Alfa == false)
+            {
+                MessageBox.Show("Řetezec je Alfanumericky, pocet Velkych je:" + pocetVelkych + "pocet malych je:" + pocetMalych + "pocet Nealfanumerickych je:" + pocetNealfa);
+            }
+            else
+            {
+                MessageBox.Show("Řetezec neni Alfanumericky, pocet Velkych je:" + pocetVelkych + "pocet malych je:" + pocetMalych + "pocet Nealfanumerickych je:" + pocetNealfa);
+            }
+        }
+    }
+}
---
+++ WindowsFormsApp15/WindowsFormsApp15/Ukol2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp15
+{
+    public partial class Ukol2 : Form
+    {
+        public Ukol2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            if (textBox1.Text.Contains("  "))
+            {
+                textBox1.Text.Replace("  ", " ");
+            }
+            string[] slova = textBox1.Text.Split(' ');
+            MazaniCisel(slova);
+        }
+
+        public void MazaniCisel(string[] slova)
+        {
+            listBox1.Items.Clear();
+            string newstr = "";
+            foreach (string s in slova)
+            {
+                string s3 = s;
+                foreach (char s2 in s3)
+                {
+                    if (char.IsDigit(s2))
+                    {
+                        s3 = s3.Replace(s2.ToString(), "");
+                    }
+                }
+                newstr += " " + s3;
+                listBox1.Items.Add(s3);
+            }
+            label1.Text = newstr.Trim();
+        }
+    }
+}
